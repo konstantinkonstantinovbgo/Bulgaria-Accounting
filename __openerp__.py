148eb20f77acca5e161c256155952d4da3b9ae12
konstantinkonstantinovbgo <konstantin.konstantinov@bgosoftware.com>
2016-03-21 11:07:42 +0200

    bg accounting

---
+++ __openerp__.py
@@ -0,0 +1,43 @@
+﻿# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2016 BGO software (<http://bgosoftware.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Bulgaria - Accounting',
+    'version': '1.0',
+    'category': 'Localization/Account',
+    'description': """
+This is the base module to manage the accounting chart for Bulgaria in OpenERP.
+    """,
+    'author': 'BGO Team',
+    'website': 'http://bgosoftware.com',
+    'images': ['static/description/icon.png'],
+    'depends': ['account'],
+    'demo': [],
+    'data': [
+        'data/account_chart.xml',
+        'data/account.account.template.csv',
+        'data/account_chart_template.xml',
+        'data/account_chart_template.yml',
+    ],
+    'sequence': 1,
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
